691a2d2834f3ce5b67d98f7be217dc179692e0fc
Bogorista <hello@bogorista.com>
2017-12-09 03:30:33 +0000

    hello

--- resources/views/vendor/backpack/base/layout.blade.php
+++ resources/views/vendor/backpack/base/layout.blade.php
@@ -15,21 +15,18 @@
     @yield('before_styles')
 
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+     <link rel="stylesheet" href="{{ mix('master/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ mix('master/css/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ mix('master/css/ionicons.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="{{ mix('master/css/admin.css') }}">
+    <link rel="stylesheet" href="{{ mix('master/css/skins.css') }}">
+    <link rel="stylesheet" href="{{ mix('master/css/custom.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/plugins/pace/pace.min.css">
-    <link rel="stylesheet" href="{{ asset('vendor/backpack/pnotify/pnotify.custom.min.css') }}">
-
-    <!-- BackPack Base CSS -->
-    <link rel="stylesheet" href="{{ asset('vendor/backpack/backpack.base.css') }}?v=2">
-    <link rel="stylesheet" href="{{ asset('vendor/backpack/overlays/backpack.bold.css') }}">
+    <link rel="stylesheet" href="{{ mix('master/css/pace.css') }}">
+    <link rel="stylesheet" href="{{ mix('master/css/pnotify.css') }}">
+    <link rel="stylesheet" href="{{ mix('master/css/base.css') }}">
+    <link rel="stylesheet" href="{{ mix('master/css/bold.css') }}">
 
     @yield('after_styles')
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,19 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+   
+mix.sass('resources/assets/backpack/bootstrap.scss', 'public/master/css')
+   .sass('resources/assets/backpack/fontawesome.scss', 'public/master/css')
+   .sass('resources/assets/backpack/ionicons.scss', 'public/master/css')
+   .sass('resources/assets/backpack/custom.scss', 'public/master/css');
+   
+mix.styles('public/vendor/adminlte/dist/css/AdminLTE.css',  'public/master/css/admin.css');
+mix.styles('public/vendor/adminlte/dist/css/skins/_all-skins.css',  'public/master/css/skins.css');
+mix.styles('public/vendor/adminlte/plugins/pace/pace.css',  'public/master/css/pace.css');
+mix.styles('public/vendor/backpack/backpack.base.css',  'public/master/css/base.css');
+mix.styles('public/vendor/backpack/overlays/backpack.bold.css',  'public/master/css/bold.css');
+
+mix.copy('public/vendor/backpack/pnotify/pnotify.custom.min.css', 'public/master/css/pnotify.css');
+mix.copy('public/vendor/backpack/ladda/ladda-themeless.min.css', 'public/master/css/ladda.css');
+mix.copy('public/vendor/backpack/ladda/spin.js', 'public/master/js/spin.js');
+mix.copy('public/vendor/backpack/ladda/ladda.js', 'public/master/js/ladda.js');
